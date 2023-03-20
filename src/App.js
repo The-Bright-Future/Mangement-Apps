9749222249459450f2ed0d0347d1ab78fe1e7e6b
Mamun8854 <mamunabdullah8854@gmail.com>
2023-03-20 11:12:56 +0600

    check push from Mamun

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ThemeProvider from './theme';
 import ScrollToTop from './components/scroll-to-top';
 import { StyledChart } from './components/chart';
 
-// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------//
 
 export default function App() {
   return (
